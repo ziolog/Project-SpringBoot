25a0815230b12cc9421001c386ef8c8e586de056
ziolog <gziolo1@stu.vistula.edu.pl>
2025-01-27 10:19:39 +0100

    Add files via upload

---
+++ src/SpringBoot/first-project-java-spring/src/test/java/pl/edu/vistula/first_project_java_spring/FirstProjectJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/SpringBoot/src/main/java/FirstProjectJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringApplication.class, args);
+	}
+
+}
